27fa7b3afc47bb7faddb81fe249d33d4d62027ca
yiri20 <yiri20@byu.edu>
2024-11-09 05:44:39 -0700

    schedule.jsx updated

---
+++ my-react-startup/src/schedule/Schedule.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Schedule.css';
+
+export default function Schedule() {
+  return (
+    <div className="schedule-container">
+      <h2>Schedule Your Music Sessions</h2>
+      <form>
+        {/* Schedule form code goes here */}
+      </form>
+    </div>
+  );
+}
