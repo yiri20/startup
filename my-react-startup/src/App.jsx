55306933bc3424dcb1c14385dabfc3cdaaa62776
yiri20 <yiri20@byu.edu>
2024-11-09 05:12:33 -0700

    Fixed infinite commit loop issue

---
+++ my-react-startup/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Plan Your Music</h1>
+      <p>Welcome to the Music Planner App!</p>
+    </div>
+  );
+}
+
+export default App;
