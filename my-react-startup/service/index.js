2636e648fe8deeef234bacb8aae804148b0007d8
yiri20 <yiri20@byu.edu>
2024-11-23 22:28:56 -0700

    mongodb package installed

--- my-react-startup/service/index.js
+++ my-react-startup/service/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Replace 'bcrypt' with 'bcryptjs'
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('./models/User');
